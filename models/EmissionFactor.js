1e0a1d99fdc84505ec8f073ee25a3866c2c0776d
Mohana Prakash <141216981+mohan-g10@users.noreply.github.com>
2026-01-02 20:35:21 +0530

    new schema added | new calculation implemented

--- models/EmissionFactor.js
+++ models/EmissionFactor.js
@@ -2,19 +2,94 @@ import mongoose from "mongoose";
 
 const EmissionFactorSchema = new mongoose.Schema(
   {
-    activity: { type: String, required: true },
-    type: { type: String },
-    size: { type: String },
-    segment: { type: String },
-    unit: { type: String, required: true },
-    value: { type: Number, required: true },
-    scope: { type: String, required: true },
-    source: { type: String, required: true },
-    year: { type: Number, required: true },
-    version: { type: String, required: true },
-    scope_id: { type: String, required: true, unique: true },
+    scope_id: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+
+    activity: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    category: {
+      type: String,
+      index: true,
+    },
+
+    size: {
+      type: String,
+      index: true,
+    },
+
+    segment: {
+      type: String,
+      index: true,
+    },
+
+    fuel: {
+      type: String,
+      index: true,
+    },
+
+    haul: {
+      type: String,
+      index: true,
+    },
+
+    class: {
+      type: String,
+      index: true,
+    },
+
+    type: {
+      type: String,
+      index: true,
+    },
+
+    distance_unit: {
+      type: String,
+      required: true,
+    },
+
+    emission_factor_value: {
+      type: Number,
+      required: true,
+    },
+
+    emission_unit: {
+      type: String,
+      required: true,
+      default: "kgCO2e",
+    },
+
+    scope: {
+      type: String,
+      required: true,
+    },
+
+    source: {
+      type: String,
+      required: true,
+    },
+
+    year: {
+      type: Number,
+      required: true,
+      index: true,
+    },
+
+    version: {
+      type: String,
+      required: true,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model(
--- services/calculation.service.js
+++ services/calculation.service.js
@@ -1,18 +1,37 @@
 import EmissionFactor from "../models/EmissionFactor.js";
 
-export const calculate = async ({ scope_id, distance }) => {
+export const calculate = async ({
+  scope_id,
+  distance,
+  trip_type,
+  passengers,
+}) => {
   const factor = await EmissionFactor.findOne({ scope_id });
 
   if (!factor) throw new Error("Emission factor not found");
 
-  const emission = distance * factor.value;
+  const tripMultiplier = trip_type === "round_trip" ? 2 : 1;
+  const distance_with_trip = distance * tripMultiplier;
+
+  const passengerMultiplier = factor.activity === "flight" ? passengers : 1;
+  console.log(
+    distance_with_trip,
+    factor.emission_factor_value,
+    passengerMultiplier
+  );
+
+  const emission =
+    distance_with_trip * factor.emission_factor_value * passengerMultiplier;
 
   return {
     scope_id,
-    distance,
+    trip_type,
+    actaul_distance: distance,
+    distance_with_trip,
     emission,
+    passengers,
     emission_unit: "kgCO2e",
-    emission_factor: factor.value,
+    emission_factor: factor.emission_factor_value,
     unit: factor.unit,
     source: factor.source,
     year: factor.year,
